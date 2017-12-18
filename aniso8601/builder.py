c010520191fab477e2a309f63c342537cb1bcdfb
Brandon Nielsen <nielsenb@jetfuse.net>
2017-12-18 15:54:38 -0600

    Add missing weeks argument.

--- aniso8601/builder.py
+++ aniso8601/builder.py
@@ -84,7 +84,7 @@ class PythonTimeBuilder(BaseTimeBuilder):
 
     @staticmethod
     def build_timedelta(days=0, seconds=0, microseconds=0, milliseconds=0, minutes=0, hours=0, weeks=0):
-        return datetime.timedelta(days=days, seconds=seconds, microseconds=microseconds, milliseconds=milliseconds, minutes=minutes, hours=hours)
+        return datetime.timedelta(days=days, seconds=seconds, microseconds=microseconds, milliseconds=milliseconds, minutes=minutes, hours=hours, weeks=weeks)
 
     @staticmethod
     def combine(date, time):
