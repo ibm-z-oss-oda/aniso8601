571baf46becaeffaf16ff61a62d47c7274faa02b
Brandon Nielsen <nielsenb@jetfuse.net>
2018-03-08 15:02:47 -0600

    Call this 3.0.0.

--- python-aniso8601.spec
+++ python-aniso8601.spec
@@ -12,7 +12,7 @@
 %global tarball_name aniso8601
 
 Name:           python-aniso8601
-Version:        2.0.2dev
+Version:        3.0.0dev
 Release:        1%{?dist}
 Summary:        Python 2 library for parsing ISO 8601 strings
 
--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ readme.close()
 
 setup(
     name='aniso8601',
-    version='2.0.2dev',
+    version='3.0.0dev',
     description='A library for parsing ISO 8601 strings.',
     long_description=README_TEXT,
     author='Brandon Nielsen',
