eea2e71d19f0bc8d093b46308f8ef44c5d96c012
Brandon Nielsen <nielsenb@jetfuse.net>
2018-06-15 09:27:30 -0500

    Prepare 3.0.1 release.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ readme.close()
 
 setup(
     name='aniso8601',
-    version='3.0.1dev0',
+    version='3.0.1',
     description='A library for parsing ISO 8601 strings.',
     long_description=README_TEXT,
     author='Brandon Nielsen',
