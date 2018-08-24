266222b067352ddc15bf204e3a89c6430e041aa5
Brandon Nielsen <nielsenb@jetfuse.net>
2018-08-24 10:15:16 -0500

    Test cleanup.

--- aniso8601/tests/test_interval.py
+++ aniso8601/tests/test_interval.py
@@ -14,72 +14,72 @@ from aniso8601.interval import parse_interval, parse_repeating_interval
 
 class TestIntervalParserFunctions(unittest.TestCase):
     def test_parse_interval(self):
-        resultinterval = parse_interval('P1M/1981-04-05T01:01:00', builder=NoneBuilder)
-        self.assertEqual(resultinterval, (None, (('1981', '04', '05', None, None, None, 'date'), ('01', '01', '00', None, 'time'), 'datetime'), (None, '1', None, None, None, None, None, 'duration'), 'interval'))
+        parse = parse_interval('P1M/1981-04-05T01:01:00', builder=NoneBuilder)
+        self.assertEqual(parse, (None, (('1981', '04', '05', None, None, None, 'date'), ('01', '01', '00', None, 'time'), 'datetime'), (None, '1', None, None, None, None, None, 'duration'), 'interval'))
 
-        resultinterval = parse_interval('P1M/1981-04-05', builder=NoneBuilder)
-        self.assertEqual(resultinterval, (None, ('1981', '04', '05', None, None, None, 'date'), (None, '1', None, None, None, None, None, 'duration'), 'interval'))
+        parse = parse_interval('P1M/1981-04-05', builder=NoneBuilder)
+        self.assertEqual(parse, (None, ('1981', '04', '05', None, None, None, 'date'), (None, '1', None, None, None, None, None, 'duration'), 'interval'))
 
-        resultinterval = parse_interval('P1.5Y/2018-03-06', builder=NoneBuilder)
-        self.assertEqual(resultinterval, (None, ('2018', '03', '06', None, None, None, 'date'), ('1.5', None, None, None, None, None, None, 'duration'), 'interval'))
+        parse = parse_interval('P1.5Y/2018-03-06', builder=NoneBuilder)
+        self.assertEqual(parse, (None, ('2018', '03', '06', None, None, None, 'date'), ('1.5', None, None, None, None, None, None, 'duration'), 'interval'))
 
-        resultinterval = parse_interval('PT1H/2014-11-12', builder=NoneBuilder)
-        self.assertEqual(resultinterval, (None, ('2014', '11', '12', None, None, None, 'date'), (None, None, None, None, '1', None, None, 'duration'), 'interval'))
+        parse = parse_interval('PT1H/2014-11-12', builder=NoneBuilder)
+        self.assertEqual(parse, (None, ('2014', '11', '12', None, None, None, 'date'), (None, None, None, None, '1', None, None, 'duration'), 'interval'))
 
-        resultinterval = parse_interval('PT4H54M6.5S/2014-11-12', builder=NoneBuilder)
-        self.assertEqual(resultinterval, (None, ('2014', '11', '12', None, None, None, 'date'), (None, None, None, None, '4', '54', '6.5', 'duration'), 'interval'))
+        parse = parse_interval('PT4H54M6.5S/2014-11-12', builder=NoneBuilder)
+        self.assertEqual(parse, (None, ('2014', '11', '12', None, None, None, 'date'), (None, None, None, None, '4', '54', '6.5', 'duration'), 'interval'))
 
-        resultinterval = parse_interval('PT0.0000001S/2018-03-06', builder=NoneBuilder)
-        self.assertEqual(resultinterval, (None, ('2018', '03', '06', None, None, None, 'date'), (None, None, None, None, None, None, '0.0000001', 'duration'), 'interval'))
+        parse = parse_interval('PT0.0000001S/2018-03-06', builder=NoneBuilder)
+        self.assertEqual(parse, (None, ('2018', '03', '06', None, None, None, 'date'), (None, None, None, None, None, None, '0.0000001', 'duration'), 'interval'))
 
-        resultinterval = parse_interval('PT2.0000048S/2018-03-06', builder=NoneBuilder)
-        self.assertEqual(resultinterval, (None, ('2018', '03', '06', None, None, None, 'date'), (None, None, None, None, None, None, '2.0000048', 'duration'), 'interval'))
+        parse = parse_interval('PT2.0000048S/2018-03-06', builder=NoneBuilder)
+        self.assertEqual(parse, (None, ('2018', '03', '06', None, None, None, 'date'), (None, None, None, None, None, None, '2.0000048', 'duration'), 'interval'))
 
-        resultinterval = parse_interval('1981-04-05T01:01:00/P1M1DT1M', builder=NoneBuilder)
-        self.assertEqual(resultinterval, ((('1981', '04', '05', None, None, None, 'date'), ('01', '01', '00', None, 'time'), 'datetime'), None, (None, '1', None, '1', None, '1', None, 'duration'), 'interval'))
+        parse = parse_interval('1981-04-05T01:01:00/P1M1DT1M', builder=NoneBuilder)
+        self.assertEqual(parse, ((('1981', '04', '05', None, None, None, 'date'), ('01', '01', '00', None, 'time'), 'datetime'), None, (None, '1', None, '1', None, '1', None, 'duration'), 'interval'))
 
-        resultinterval = parse_interval('1981-04-05/P1M1D', builder=NoneBuilder)
-        self.assertEqual(resultinterval, (('1981', '04', '05', None, None, None, 'date'), None, (None, '1', None, '1', None, None, None, 'duration'), 'interval'))
+        parse = parse_interval('1981-04-05/P1M1D', builder=NoneBuilder)
+        self.assertEqual(parse, (('1981', '04', '05', None, None, None, 'date'), None, (None, '1', None, '1', None, None, None, 'duration'), 'interval'))
 
-        resultinterval = parse_interval('2018-03-06/P2.5M', builder=NoneBuilder)
-        self.assertEqual(resultinterval, (('2018', '03', '06', None, None, None, 'date'), None, (None, '2.5', None, None, None, None, None, 'duration'), 'interval'))
+        parse = parse_interval('2018-03-06/P2.5M', builder=NoneBuilder)
+        self.assertEqual(parse, (('2018', '03', '06', None, None, None, 'date'), None, (None, '2.5', None, None, None, None, None, 'duration'), 'interval'))
 
-        resultinterval = parse_interval('2014-11-12/PT1H', builder=NoneBuilder)
-        self.assertEqual(resultinterval, (('2014', '11', '12', None, None, None, 'date'), None, (None, None, None, None, '1', None, None, 'duration'), 'interval'))
+        parse = parse_interval('2014-11-12/PT1H', builder=NoneBuilder)
+        self.assertEqual(parse, (('2014', '11', '12', None, None, None, 'date'), None, (None, None, None, None, '1', None, None, 'duration'), 'interval'))
 
-        resultinterval = parse_interval('2014-11-12/PT4H54M6.5S', builder=NoneBuilder)
-        self.assertEqual(resultinterval, (('2014', '11', '12', None, None, None, 'date'), None, (None, None, None, None, '4', '54', '6.5', 'duration'), 'interval'))
+        parse = parse_interval('2014-11-12/PT4H54M6.5S', builder=NoneBuilder)
+        self.assertEqual(parse, (('2014', '11', '12', None, None, None, 'date'), None, (None, None, None, None, '4', '54', '6.5', 'duration'), 'interval'))
 
-        resultinterval = parse_interval('2018-03-06/PT0.0000001S', builder=NoneBuilder)
-        self.assertEqual(resultinterval, (('2018', '03', '06', None, None, None, 'date'), None, (None, None, None, None, None, None, '0.0000001', 'duration'), 'interval'))
+        parse = parse_interval('2018-03-06/PT0.0000001S', builder=NoneBuilder)
+        self.assertEqual(parse, (('2018', '03', '06', None, None, None, 'date'), None, (None, None, None, None, None, None, '0.0000001', 'duration'), 'interval'))
 
-        resultinterval = parse_interval('2018-03-06/PT2.0000048S', builder=NoneBuilder)
-        self.assertEqual(resultinterval, (('2018', '03', '06', None, None, None, 'date'), None, (None, None, None, None, None, None, '2.0000048', 'duration'), 'interval'))
+        parse = parse_interval('2018-03-06/PT2.0000048S', builder=NoneBuilder)
+        self.assertEqual(parse, (('2018', '03', '06', None, None, None, 'date'), None, (None, None, None, None, None, None, '2.0000048', 'duration'), 'interval'))
 
-        resultinterval = parse_interval('1980-03-05T01:01:00/1981-04-05T01:01:00', builder=NoneBuilder)
-        self.assertEqual(resultinterval, ((('1980', '03', '05', None, None, None, 'date'), ('01', '01', '00', None, 'time'), 'datetime'), (('1981', '04', '05', None, None, None, 'date'), ('01', '01', '00', None, 'time'), 'datetime'), None, 'interval'))
+        parse = parse_interval('1980-03-05T01:01:00/1981-04-05T01:01:00', builder=NoneBuilder)
+        self.assertEqual(parse, ((('1980', '03', '05', None, None, None, 'date'), ('01', '01', '00', None, 'time'), 'datetime'), (('1981', '04', '05', None, None, None, 'date'), ('01', '01', '00', None, 'time'), 'datetime'), None, 'interval'))
 
-        resultinterval = parse_interval('1980-03-05T01:01:00/1981-04-05', builder=NoneBuilder)
-        self.assertEqual(resultinterval, ((('1980', '03', '05', None, None, None, 'date'), ('01', '01', '00', None, 'time'), 'datetime'), ('1981', '04', '05', None, None, None, 'date'), None, 'interval'))
+        parse = parse_interval('1980-03-05T01:01:00/1981-04-05', builder=NoneBuilder)
+        self.assertEqual(parse, ((('1980', '03', '05', None, None, None, 'date'), ('01', '01', '00', None, 'time'), 'datetime'), ('1981', '04', '05', None, None, None, 'date'), None, 'interval'))
 
-        resultinterval = parse_interval('1980-03-05/1981-04-05T01:01:00', builder=NoneBuilder)
-        self.assertEqual(resultinterval, (('1980', '03', '05', None, None, None, 'date'), (('1981', '04', '05', None, None, None, 'date'), ('01', '01', '00', None, 'time'), 'datetime'), None, 'interval'))
+        parse = parse_interval('1980-03-05/1981-04-05T01:01:00', builder=NoneBuilder)
+        self.assertEqual(parse, (('1980', '03', '05', None, None, None, 'date'), (('1981', '04', '05', None, None, None, 'date'), ('01', '01', '00', None, 'time'), 'datetime'), None, 'interval'))
 
-        resultinterval = parse_interval('1980-03-05/1981-04-05', builder=NoneBuilder)
-        self.assertEqual(resultinterval, (('1980', '03', '05', None, None, None, 'date'), ('1981', '04', '05', None, None, None, 'date'), None, 'interval'))
+        parse = parse_interval('1980-03-05/1981-04-05', builder=NoneBuilder)
+        self.assertEqual(parse, (('1980', '03', '05', None, None, None, 'date'), ('1981', '04', '05', None, None, None, 'date'), None, 'interval'))
 
-        resultinterval = parse_interval('1981-04-05/1980-03-05', builder=NoneBuilder)
-        self.assertEqual(resultinterval, (('1981', '04', '05', None, None, None, 'date'), ('1980', '03', '05', None, None, None, 'date'), None, 'interval'))
+        parse = parse_interval('1981-04-05/1980-03-05', builder=NoneBuilder)
+        self.assertEqual(parse, (('1981', '04', '05', None, None, None, 'date'), ('1980', '03', '05', None, None, None, 'date'), None, 'interval'))
 
-        resultinterval = parse_interval('1980-03-05T01:01:00.0000001/1981-04-05T14:43:59.9999997', builder=NoneBuilder)
-        self.assertEqual(resultinterval, ((('1980', '03', '05', None, None, None, 'date'), ('01', '01', '00.0000001', None, 'time'), 'datetime'), (('1981', '04', '05', None, None, None, 'date'), ('14', '43', '59.9999997', None, 'time'), 'datetime'), None, 'interval'))
+        parse = parse_interval('1980-03-05T01:01:00.0000001/1981-04-05T14:43:59.9999997', builder=NoneBuilder)
+        self.assertEqual(parse, ((('1980', '03', '05', None, None, None, 'date'), ('01', '01', '00.0000001', None, 'time'), 'datetime'), (('1981', '04', '05', None, None, None, 'date'), ('14', '43', '59.9999997', None, 'time'), 'datetime'), None, 'interval'))
 
         #Test different separators
-        resultinterval = parse_interval('1980-03-05T01:01:00--1981-04-05T01:01:00', intervaldelimiter='--', builder=NoneBuilder)
-        self.assertEqual(resultinterval, ((('1980', '03', '05', None, None, None, 'date'), ('01', '01', '00', None, 'time'), 'datetime'), (('1981', '04', '05', None, None, None, 'date'), ('01', '01', '00', None, 'time'), 'datetime'), None, 'interval'))
+        parse = parse_interval('1980-03-05T01:01:00--1981-04-05T01:01:00', intervaldelimiter='--', builder=NoneBuilder)
+        self.assertEqual(parse, ((('1980', '03', '05', None, None, None, 'date'), ('01', '01', '00', None, 'time'), 'datetime'), (('1981', '04', '05', None, None, None, 'date'), ('01', '01', '00', None, 'time'), 'datetime'), None, 'interval'))
 
-        resultinterval = parse_interval('1980-03-05 01:01:00/1981-04-05 01:01:00', datetimedelimiter=' ', builder=NoneBuilder)
-        self.assertEqual(resultinterval, ((('1980', '03', '05', None, None, None, 'date'), ('01', '01', '00', None, 'time'), 'datetime'), (('1981', '04', '05', None, None, None, 'date'), ('01', '01', '00', None, 'time'), 'datetime'), None, 'interval'))
+        parse = parse_interval('1980-03-05 01:01:00/1981-04-05 01:01:00', datetimedelimiter=' ', builder=NoneBuilder)
+        self.assertEqual(parse, ((('1980', '03', '05', None, None, None, 'date'), ('01', '01', '00', None, 'time'), 'datetime'), (('1981', '04', '05', None, None, None, 'date'), ('01', '01', '00', None, 'time'), 'datetime'), None, 'interval'))
 
     def test_parse_interval_repeating(self):
         #Parse interval can't parse repeating intervals
@@ -109,20 +109,20 @@ class TestIntervalParserFunctions(unittest.TestCase):
 
 class TestRepeatingIntervalParserFunctions(unittest.TestCase):
     def test_parse_repeating_interval(self):
-        results = parse_repeating_interval('R3/1981-04-05/P1D', builder=NoneBuilder)
-        self.assertEqual(results, (False, '3', (('1981', '04', '05', None, None, None, 'date'), None, (None, None, None, '1', None, None, None, 'duration'), 'interval'), 'repeatinginterval'))
+        parse = parse_repeating_interval('R3/1981-04-05/P1D', builder=NoneBuilder)
+        self.assertEqual(parse, (False, '3', (('1981', '04', '05', None, None, None, 'date'), None, (None, None, None, '1', None, None, None, 'duration'), 'interval'), 'repeatinginterval'))
 
-        results = parse_repeating_interval('R11/PT1H2M/1980-03-05T01:01:00', builder=NoneBuilder)
-        self.assertEqual(results, (False, '11', (None, (('1980', '03', '05', None, None, None, 'date'), ('01', '01', '00', None, 'time'), 'datetime'), (None, None, None, None, '1', '2', None, 'duration'), 'interval'), 'repeatinginterval'))
+        parse = parse_repeating_interval('R11/PT1H2M/1980-03-05T01:01:00', builder=NoneBuilder)
+        self.assertEqual(parse, (False, '11', (None, (('1980', '03', '05', None, None, None, 'date'), ('01', '01', '00', None, 'time'), 'datetime'), (None, None, None, None, '1', '2', None, 'duration'), 'interval'), 'repeatinginterval'))
 
-        results = parse_repeating_interval('R2--1980-03-05T01:01:00--1981-04-05T01:01:00', intervaldelimiter='--', builder=NoneBuilder)
-        self.assertEqual(results, (False, '2', ((('1980', '03', '05', None, None, None, 'date'), ('01', '01', '00', None, 'time'), 'datetime'), (('1981', '04', '05', None, None, None, 'date'), ('01', '01', '00', None, 'time'), 'datetime'), None, 'interval'), 'repeatinginterval'))
+        parse = parse_repeating_interval('R2--1980-03-05T01:01:00--1981-04-05T01:01:00', intervaldelimiter='--', builder=NoneBuilder)
+        self.assertEqual(parse, (False, '2', ((('1980', '03', '05', None, None, None, 'date'), ('01', '01', '00', None, 'time'), 'datetime'), (('1981', '04', '05', None, None, None, 'date'), ('01', '01', '00', None, 'time'), 'datetime'), None, 'interval'), 'repeatinginterval'))
 
-        results = parse_repeating_interval('R2/1980-03-05 01:01:00/1981-04-05 01:01:00', datetimedelimiter=' ', builder=NoneBuilder)
-        self.assertEqual(results, (False, '2', ((('1980', '03', '05', None, None, None, 'date'), ('01', '01', '00', None, 'time'), 'datetime'), (('1981', '04', '05', None, None, None, 'date'), ('01', '01', '00', None, 'time'), 'datetime'), None, 'interval'), 'repeatinginterval'))
+        parse = parse_repeating_interval('R2/1980-03-05 01:01:00/1981-04-05 01:01:00', datetimedelimiter=' ', builder=NoneBuilder)
+        self.assertEqual(parse, (False, '2', ((('1980', '03', '05', None, None, None, 'date'), ('01', '01', '00', None, 'time'), 'datetime'), (('1981', '04', '05', None, None, None, 'date'), ('01', '01', '00', None, 'time'), 'datetime'), None, 'interval'), 'repeatinginterval'))
 
-        resultgenerator = parse_repeating_interval('R/PT1H2M/1980-03-05T01:01:00', builder=NoneBuilder)
-        self.assertEqual(resultgenerator, (True, None, (None, (('1980', '03', '05', None, None, None, 'date'), ('01', '01', '00', None, 'time'), 'datetime'), (None, None, None, None, '1', '2', None, 'duration'), 'interval'), 'repeatinginterval'))
+        parse = parse_repeating_interval('R/PT1H2M/1980-03-05T01:01:00', builder=NoneBuilder)
+        self.assertEqual(parse, (True, None, (None, (('1980', '03', '05', None, None, None, 'date'), ('01', '01', '00', None, 'time'), 'datetime'), (None, None, None, None, '1', '2', None, 'duration'), 'interval'), 'repeatinginterval'))
 
     def test_parse_repeating_interval_suffixgarbage(self):
         #Don't allow garbage after the duration
