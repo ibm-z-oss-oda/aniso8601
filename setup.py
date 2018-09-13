abcdf8ab9e30b46802e59e56a9a672cdd562f662
Brandon Nielsen <nielsenb@jetfuse.net>
2018-09-13 14:39:25 -0500

    Mark this as the base of 4.0.0

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ readme.close()
 
 setup(
     name='aniso8601',
-    version='3.0.3dev0',
+    version='4.0.0dev0',
     description='A library for parsing ISO 8601 strings.',
     long_description=README_TEXT,
     author='Brandon Nielsen',
