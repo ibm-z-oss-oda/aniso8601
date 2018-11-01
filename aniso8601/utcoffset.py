c976ec1120ebff8cb0e325f803ccd4c4e0d52235
Brandon Nielsen <nielsenb@jetfuse.net>
2018-11-01 10:30:08 -0500

    Use separate `relativetimebuilder` project instead of built in builder.

--- aniso8601/utcoffset.py
+++ aniso8601/utcoffset.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
 
+<<<<<<< HEAD
 # Copyright (c) 2019, Brandon Nielsen
+=======
+# Copyright (c) 2018, Brandon Nielsen
+>>>>>>> Use separate `relativetimebuilder` project instead of built in builder.
 # All rights reserved.
 #
 # This software may be modified and distributed under the terms
