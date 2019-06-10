88e3a159b6a8b7cb93df78fc9ba2cb77425859bb
Brandon Nielsen <nielsenb@jetfuse.net>
2019-06-10 14:37:30 -0500

    Additional _distribute_microseconds test.

--- aniso8601/builders/tests/test_python.py
+++ aniso8601/builders/tests/test_python.py
@@ -992,3 +992,7 @@ class TestPythonTimeBuilder(unittest.TestCase):
         self.assertEqual(PythonTimeBuilder._distribute_microseconds(1, (), ()), (1,))
         self.assertEqual(PythonTimeBuilder._distribute_microseconds(11, (0,), (10,)), (1, 1))
         self.assertEqual(PythonTimeBuilder._distribute_microseconds(211, (0, 0), (100, 10)), (2, 1, 1))
+
+        self.assertEqual(PythonTimeBuilder._distribute_microseconds(1, (), ()), (1,))
+        self.assertEqual(PythonTimeBuilder._distribute_microseconds(11, (5,), (10,)), (6, 1))
+        self.assertEqual(PythonTimeBuilder._distribute_microseconds(211, (10, 5), (100, 10)), (12, 6, 1))
