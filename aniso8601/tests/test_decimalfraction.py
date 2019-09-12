fc2cf46dd23403f17031abc5ff94dfd7b3a77afc
Brandon Nielsen <nielsenb@jetfuse.net>
2019-09-11 19:55:56 -0500

    Add missing license.

--- aniso8601/tests/test_decimalfraction.py
+++ aniso8601/tests/test_decimalfraction.py
@@ -1,7 +1,14 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2019, Brandon Nielsen
+# All rights reserved.
+#
+# This software may be modified and distributed under the terms
+# of the BSD license.  See the LICENSE file for details.
+
 import unittest
 
 from aniso8601.decimalfraction import find_separator, normalize
-
 
 class TestDecimalFractionFunctions(unittest.TestCase):
     def test_find_separator(self):
