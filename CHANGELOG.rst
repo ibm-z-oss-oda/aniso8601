0ec13b9d81c9c34a915e05a69a910abb359b4282
Brandon Nielsen <nielsenb@jetfuse.net>
2019-09-11 19:57:27 -0500

    Prepare 8.0.0 release.

--- CHANGELOG.rst
+++ CHANGELOG.rst
@@ -1,10 +1,10 @@
 Changelog
 =========
 
-aniso8601 8.0.0-dev.0
-=====================
+aniso8601 8.0.0
+===============
 
-*Release date: YYYY-MM-DD*
+*Release date: 2019-09-11*
 
 Changes
 -------
--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ readme.close()
 
 setup(
     name='aniso8601',
-    version='8.0.0-dev.0',
+    version='8.0.0',
     description='A library for parsing ISO 8601 strings.',
     long_description=README_TEXT,
     author='Brandon Nielsen',
