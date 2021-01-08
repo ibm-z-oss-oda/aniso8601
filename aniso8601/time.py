ca7f702cb5bacfffdaa815cc8a2881b5cf31e2a3
Brandon Nielsen <nielsenb@jetfuse.net>
2021-01-07 20:30:38 -0600

    Unused import.

--- aniso8601/time.py
+++ aniso8601/time.py
@@ -10,7 +10,7 @@ from aniso8601.builders import TupleBuilder
 from aniso8601.builders.python import PythonTimeBuilder
 from aniso8601.compat import is_string
 from aniso8601.date import parse_date
-from aniso8601.decimalfraction import find_separator, normalize
+from aniso8601.decimalfraction import normalize
 from aniso8601.exceptions import ISOFormatError
 from aniso8601.resolution import TimeResolution
 from aniso8601.timezone import parse_timezone
