6d2e450d9d3a9179e9c74a3d072ecd583f7a3389
Brandon Nielsen <nielsenb@jetfuse.net>
2021-01-29 18:12:28 -0600

    Add _parse_interval_end tests.

--- aniso8601/tests/test_interval.py
+++ aniso8601/tests/test_interval.py
@@ -13,8 +13,8 @@ from aniso8601.builders import (DateTuple, DatetimeTuple,
                                 DurationTuple, IntervalTuple, TimeTuple,
                                 TimezoneTuple)
 from aniso8601.exceptions import ISOFormatError
-from aniso8601.interval import (_parse_interval, parse_interval,
-                                parse_repeating_interval)
+from aniso8601.interval import (_parse_interval, _parse_interval_end,
+                                parse_interval, parse_repeating_interval)
 from aniso8601.tests.compat import mock
 
 class TestIntervalParserFunctions(unittest.TestCase):
@@ -587,6 +587,47 @@ class TestIntervalParserFunctions(unittest.TestCase):
 
         self.assertEqual(result, expectedargs)
         mockBuilder.build_interval.assert_called_once_with(**expectedargs)
+
+    def test_parse_interval_end(self):
+        self.assertEqual(_parse_interval_end('02', DateTuple('2020', '01', '01',
+                                                             None, None, None),
+                                             'T'),
+                                             DateTuple(None, None, '02',
+                                                       None, None, None))
+
+        self.assertEqual(_parse_interval_end('03-14', DateTuple('2008', '02', '15',
+                                                                None, None, None),
+                                             'T'),
+                                             DateTuple(None, '03', '14',
+                                                       None, None, None))
+
+        self.assertEqual(_parse_interval_end('15:30', DatetimeTuple(DateTuple('2007', '12', '14',
+                                                                              None, None, None),
+                                                                    TimeTuple('13', '30', None,
+                                                                              None)),
+                                             'T'),
+                                             TimeTuple('15', '30', None,
+                                                       None))
+
+        self.assertEqual(_parse_interval_end('15T17:00', DatetimeTuple(DateTuple('2007', '11', '13',
+                                                                                 None, None, None),
+                                                                       TimeTuple('09', '00', None,
+                                                                                 None)),
+                                             'T'),
+                                             DatetimeTuple(DateTuple(None, None, '15',
+                                                                     None, None, None),
+                                                           TimeTuple('17', '00', None,
+                                                                     None)))
+
+        self.assertEqual(_parse_interval_end('16T00:00', DatetimeTuple(DateTuple('2007', '11', '13',
+                                                                                 None, None, None),
+                                                                       TimeTuple('00', '00', None,
+                                                                                 None)),
+                                             'T'),
+                                             DatetimeTuple(DateTuple(None, None, '16',
+                                                                     None, None, None),
+                                                           TimeTuple('00', '00', None,
+                                                                     None)))
 
 class TestRepeatingIntervalParserFunctions(unittest.TestCase):
     def test_parse_repeating_interval(self):
