2ec77a650ea6f6174b3e435108a613677b1134cd
Brandon Nielsen <nielsenb@jetfuse.net>
2021-02-01 21:13:40 -0600

    Improve range_check_duration tests.

--- aniso8601/builders/tests/test_python.py
+++ aniso8601/builders/tests/test_python.py
@@ -911,22 +911,37 @@ class TestPythonTimeBuilder(unittest.TestCase):
 
     def test_range_check_duration(self):
         with self.assertRaises(YearOutOfBoundsError):
-            PythonTimeBuilder.build_duration(PnY='3333333')
+            PythonTimeBuilder.build_duration(PnY=str((datetime.timedelta.max.days // 365) + 1))
 
         with self.assertRaises(MonthOutOfBoundsError):
-            PythonTimeBuilder.build_duration(PnM='2000000000000000')
-
-        with self.assertRaises(MonthOutOfBoundsError):
-            PythonTimeBuilder.build_duration(PnM='33333333.9')
-
-        with self.assertRaises(WeekOutOfBoundsError):
-            PythonTimeBuilder.build_duration(PnW='9999999999999999999999')
+            PythonTimeBuilder.build_duration(PnM=str((datetime.timedelta.max.days // 30) + 1))
 
         with self.assertRaises(DayOutOfBoundsError):
-            PythonTimeBuilder.build_duration(TnH='5511111511', TnM='1111177111777')
+            PythonTimeBuilder.build_duration(PnD=str(datetime.timedelta.max.days + 1))
+
+        with self.assertRaises(WeekOutOfBoundsError):
+            PythonTimeBuilder.build_duration(PnW=str((datetime.timedelta.max.days // 7) + 1))
+
+        with self.assertRaises(HoursOutOfBoundsError):
+            PythonTimeBuilder.build_duration(TnH=str((datetime.timedelta.max.days * 24) + 24))
 
         with self.assertRaises(MinutesOutOfBoundsError):
-            PythonTimeBuilder.build_duration(PnM='666', TnM='66666666666666666666666666666')
+            PythonTimeBuilder.build_duration(TnM=str((datetime.timedelta.max.days * 24 * 60) + 24 * 60))
+
+        with self.assertRaises(SecondsOutOfBoundsError):
+            PythonTimeBuilder.build_duration(TnS=str((datetime.timedelta.max.days * 24 * 60 * 60) + 24 * 60 * 60))
+
+        #Split max range across all parts
+        maxpart = datetime.timedelta.max.days // 7
+
+        with self.assertRaises(DayOutOfBoundsError):
+            PythonTimeBuilder.build_duration(PnY=str((maxpart // 365) + 1),
+                                             PnM=str((maxpart // 30) + 1),
+                                             PnD=str((maxpart + 1)),
+                                             PnW=str((maxpart // 7) + 1),
+                                             TnH=str((maxpart * 24) + 1),
+                                             TnM=str((maxpart * 24 * 60) + 1),
+                                             TnS=str((maxpart * 24* 60 * 60) + 1))
 
     def test_range_check_interval(self):
         with self.assertRaises(YearOutOfBoundsError):
